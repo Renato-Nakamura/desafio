406567c85c3be0bcf1e90968184514baf0b49443
Renato-Nakamura <renato.yukio30@hotmail.com>
2021-12-07 20:11:47 -0300

    Parte 2, fase 1 e 2

--- adm/src/app/pages/cadastro-ofertas/cadastro-ofertas.component.ts
+++ adm/src/app/pages/cadastro-ofertas/cadastro-ofertas.component.ts
@@ -1,21 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-cadastro-ofertas',
-  templateUrl: './cadastro-ofertas.component.html',
-  styleUrls: ['./cadastro-ofertas.component.scss']
+  selector: "app-cadastro-ofertas",
+  templateUrl: "./cadastro-ofertas.component.html",
+  styleUrls: ["./cadastro-ofertas.component.scss"],
 })
 export class CadastroOfertasComponent implements OnInit {
-
   lojas = [
-    { id: 1, nome: 'Epic' },
-    { id: 2, nome: 'Origin' },
-    { id: 3, nome: 'Steam' },
+    { id: 1, nome: "Epic" },
+    { id: 2, nome: "Origin" },
+    { id: 3, nome: "Steam" },
   ];
 
-  constructor() { }
+  gameInfo: {
+    id: number;
+    titulo: string;
+    preco: string;
+    precoDesconto: string;
+  };
+
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    let id = this.route.snapshot.params["id"];
+    this.gameInfo = this.getGameInformation(id);
+    console.log(id, this.gameInfo);
   }
 
+  getGameInformation(id:number) {
+    let data = JSON.parse(window.localStorage.getItem("ofertas-game-tracker"));
+    return data.find((game) => game.id == id);
+  }
 }
--- adm/src/app/pages/nossas-ofertas/nossas-ofertas.component.ts
+++ adm/src/app/pages/nossas-ofertas/nossas-ofertas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nossas-ofertas',
@@ -11,10 +12,19 @@ export class NossasOfertasComponent implements OnInit {
   displayedColumns: string[] = ['id', 'titulo', 'preco', 'precoDesconto'];
   dataSource;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.dataSource = JSON.parse(window.localStorage.getItem("ofertas-game-tracker"));
+  }
+
+  rowClick(id:number){
+    this.router.navigate([`/cadastroofertas/${id}`])
+  }
+  newOffer(){
+    this.router.navigate([`/cadastroofertas/new`])
   }
 
 }
